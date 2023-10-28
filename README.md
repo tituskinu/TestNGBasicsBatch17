76c4c7ae4634126439b9ab4215756938b92138f9
tituskinu <mwaura.tk@gmail.com>
2023-10-28 08:10:54 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# TestNGBasicsBatch17
---
+++ src/test/java/class01/TestNGAnnotations.java
@@ -0,0 +1,26 @@
+package class01;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestNGAnnotations {
+
+    @BeforeMethod
+    public void beforeMethod(){
+        System.out.println("i am before method");
+    }
+    @Test
+    public void aTestCase(){
+        System.out.println("i am the first test case");
+    }
+    @Test
+    public void bTestCase(){
+        System.out.println("i am the second test case");
+    }
+    @AfterMethod
+    public void afterMethod(){
+        System.out.println("i am after method");
+    }
+
+}
